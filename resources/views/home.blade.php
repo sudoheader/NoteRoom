d669389662a1ea01b6ba9cfb43ce8ca0ec236a8e
novellito <christian.trinidad.501@my.csun.edu>
2017-03-21 10:42:17 -0700

    create basic layout without text editor

---
+++ resources/views/home.blade.php
@@ -0,0 +1,123 @@
+<!doctype html>
+<html>
+  <head>
+    <title>Socket.IO chat</title>
+
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+
+    <style>
+          body{margin-top:50px;}
+      .glyphicon { margin-right:10px; }
+      .panel-body { padding:0px; }
+      .panel-body table tr td { padding-left: 15px }
+      .panel-body .table {margin-bottom: 0px; }
+
+    </style>
+
+  </head>
+
+  <body>
+
+<div class="container">
+    <div class="row">
+        <div class="col-sm-3 col-md-3">
+            <div class="panel-group" id="accordion">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h4 class="panel-title">
+                            <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne"><span class="glyphicon glyphicon-folder-close">
+                            </span>My NoteRooms</a>
+                        </h4>
+                    </div>
+                    <div id="collapseOne" class="panel-collapse collapse in">
+                        <div class="panel-body">
+                            <table class="table">
+                                <tr>
+                                    <td>
+                                        <span class="glyphicon glyphicon-pencil text-primary"></span><a href="http://www.jquery2dotnet.com">comp380</a>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <span class="glyphicon glyphicon-flash text-success"></span><a href="http://www.jquery2dotnet.com">comp333</a>
+                                    </td>
+                                </tr>
+                                
+                            </table>
+                        </div>
+                    </div>
+                </div>
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h4 class="panel-title">
+                            <a data-toggle="collapse" data-parent="#accordion" href="#collapseTwo"><span class="glyphicon glyphicon-th">
+                            </span>Join NoteRoom</a>
+                        </h4>
+                    </div>
+                    
+                </div>
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h4 class="panel-title">
+                            <a data-toggle="collapse" data-parent="#accordion" href="#collapseThree"><span class="glyphicon glyphicon-user">
+                            </span>Create NoteRoom</a>
+                        </h4>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+
+
+        <div class="col-sm-9 col-md-9">
+            <div class="well">
+                <h1>
+                    Accordion Menu With Icon</h1>
+                Admin Dashboard Accordion Menu
+            </div>
+
+          <form action="">
+
+        <label for="question">Ask a question</label>
+        <input id="question" autocomplete="off"  placeholder="Enter question"/> <button type="submit" class="btn btn-primary">Submit</button>
+
+    </form>
+
+
+
+        </div>
+    </div>
+</div>
+
+
+    <div class="well well-lg">
+
+        <div id="messages">
+            
+        </div>
+    
+    </div>
+    
+
+    
+
+
+
+
+
+
+
+
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    <script src="\NoteRoom\node_modules\socket.io-client\dist\socket.io.js"></script>
+    <script src="https://code.jquery.com/jquery-1.11.1.js"></script>
+    <script src = "\NoteRoom\public\js\client.js"></script>
+
+
+  </body>
+</html>
