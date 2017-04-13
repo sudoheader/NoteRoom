b47f0640deb598713c8bb28d1d4c440f8659df4f
Gabriel Alabastro <gabriel.alabastro@gmail.com>
2017-04-12 19:23:05 -0700

    cleanup

--- resources/views/notes.blade.php
+++ resources/views/notes.blade.php
@@ -2,82 +2,38 @@
     
 @section('title', 'Notes Page')
 
-   
+@section('styles')
+  <link rel= "stylesheet" href="\NoteRoom\public\css\myNotes.css">
+  <link href="https://cdn.quilljs.com/1.0.0/quill.snow.css" rel="stylesheet">
+@endsection
 
-@section('content')
-
-    
-    <link rel= "stylesheet" href="\NoteRoom\public\css\myNotes.css">
-   
-    <link href="https://cdn.quilljs.com/1.0.0/quill.snow.css" rel="stylesheet">
-
-    <style>
-         /* body{margin-top:50px;}
-      .glyphicon { margin-right:10px; }
-      .panel-body { padding:0px; }
-      .pan  el-body table tr td { padding-left: 15px }
-      .panel-body .table {margin-bottom: 0px; }*/
-    </style>
-
- 
+@section('content') 
     <div class="container text-center"> 
-
         <div class="row content">
-        
             <div class="col-sm-9 text-left"> 
-
               <div id="editor">
-                              
               </div>
-
             </div>
-
             <div class="col-sm-3 sidenav">
-
               <div class="well">
-
               <label for="question">Ask a question</label>
-                
-                 <form action="">
-
+                  <form action="">
                       <input id="question" autocomplete="off"  placeholder="Enter question"/> <button type="submit" class="btn-sm btn-primary">Submit</button>
-
-                 </form>
-            
+                  </form>
                 <p><u>Questions</u></p> 
-
                   <div id="messages">
-            
                   </div>
-
               </div>
-  
-              
             </div>
-
         </div>
-
-
     </div>
-
-
     <footer class="container-fluid text-center">
-
       <p>NoteRoom</p>
-
     </footer>
-
-
   <script src="https://cdn.quilljs.com/1.0.0/quill.js"></script>
- 
-   
-    
   <script src="\NoteRoom\node_modules\socket.io-client\dist\socket.io.js"></script>
-
      <!-- Initialize Quill editor -->
   <script src = "\NoteRoom\public\js\quillCode.js"></script> 
-    
    <!-- {{--  temporarily commented to separate ask a question function from quill stuff - will need to integrate later... --}} -->
   <!--  <script src = "\NoteRoom\public\js\client.js"></script> -->
-
 @stop
